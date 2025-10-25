3af5867f7732ba4e1573437e245027ae95684bc9
Jacob J <jirak87@outlook.com>
2025-10-24 23:38:48 -0500

    Got Vite working. Needs tuning.

--- AfAFrontend/vite.config.js
+++ AfAFrontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
-// docs say to have this nxt import; still trying to get it to work
+// docs say to have this next import; still trying to get it to work
 // import 'vite/modulepreload-polyfill'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
