3517416d0f83c2ae72eceb832f82cd3ac8f79823
Jacob J <jirak87@outlook.com>
2025-10-24 23:38:45 -0500

    Removed unnecessary boilerplate Windows code.

--- app.py
+++ app.py
@@ -420,14 +420,3 @@ def displaywork(artistpage, worktitle):
     return render_template("text.html", 
                            artistpage=subdomain.name, worktitle=worktitle, 
                            artist=subdomain.getArtistName(), htmlText=htmlText)
-
-
-# Windows-provided default Python Flask app code
-if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
